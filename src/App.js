b9c3d5425dc81185d534b93207674a1e3095e1df
unknown <avpyrta@bk.ru>
2021-03-08 14:34:32 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Zar Zakharov</h1>
+        <h1>Hello Zar Zakharov!</h1>
         <p>
           It's my first React.js app
         </p>
