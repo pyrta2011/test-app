9425d23247e9026579c2ff2d9e8d0b8637ddd8b5
unknown <avpyrta@bk.ru>
2021-03-08 14:29:32 +0400

    Commit
    Test Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Zar Zakharov</h1>
+        <p>
+          It's my first React.js app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
