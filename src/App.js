f1493e9e1dfb988b332bff0ca469ad3085386097
unknown <avpyrta@bk.ru>
2021-03-10 23:27:55 +0400

    Homework 2
    Я сделал, но не уверен что все правильно, с чем возникла сложность:
    во первых не совсем понял как в компоненте Layout, для элемента с описанием задать два класса (class="desc full"), и момент как проверить пропсы на путь картинок и применить необходимые стили.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
 
-function App() {
+import bg from './assets/bg.jpg';
+import back from './assets/back.jpg';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Zar Zakharov!</h1>
-        <p>
-          It's my first React.js app
-        </p>
-      </header>
-    </div>
+    <>
+        <Header title={'Hello'} desc={'Hi from React Component'}/>
+        <Layout title={'Firts Component'} desc={'This is first React Component'} urlBg={bg} />
+        <Layout title={'Second Component'} desc={'This is second React Component'}  colorBg={'#e2e2e2'}/>
+        <Layout title={'Three Component'} desc={'This is third React Component'} urlBg={back} />
+        <Footer />
+    </>
   );
-}
+};
 
 export default App;
